591140024ea883c3fa77d4c556cddde746c373df
Till Wehowski <github@webfan.de>
2021-10-13 22:46:14 +0200

    Create Wrapper.php

---
+++ webfan-module-shares/CodeHub/Client/Wrapper.php
@@ -0,0 +1,15 @@
+<?php
+namespace VersionAgnostic\VendorPatch\EmbeddedComposer\Webfantized\CodeHub\Client;
+
+class Wrapper
+{
+  const CIRCUIT = 0;
+  const RUNTIME = 1;
+  
+  public function register(bool|int $prepend = static::CIRCUIT | static::RUNTIME){
+    
+  }
+  
+}
+
+
